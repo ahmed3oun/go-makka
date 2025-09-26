24eed2c5b7d172a61e55859b3ef49ba65bd96842
Ahmed OUN <72476024+ahmed3oun@users.noreply.github.com>
2025-09-26 18:05:37 +0100

    Update index.ts

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -18,8 +18,8 @@ if (process.env.NODE_ENV === "development") {
 }
 console.log("Database URL:", process.env.DATABASE_URL || process.env.POSTGRES_URL);
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://wuuuqnvhvrkdwmdcmulj.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dXVxbnZodnJrZHdtZGNtdWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDgyMDksImV4cCI6MjA3MzkyNDIwOX0.PUZk8iT9we0EdXwAbxASxUH12a-p9DbRBxYJWMkV528'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const client = postgres(process.env.DATABASE_URL!, { prepare: false })
