02b6ae9ba24964d12c446f877786896a9c1a783b
Ahmed OUN <72476024+ahmed3oun@users.noreply.github.com>
2025-09-26 18:06:14 +0100

    Update client.ts

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://wuuuqnvhvrkdwmdcmulj.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dXVxbnZodnJrZHdtZGNtdWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDgyMDksImV4cCI6MjA3MzkyNDIwOX0.PUZk8iT9we0EdXwAbxASxUH12a-p9DbRBxYJWMkV528'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
